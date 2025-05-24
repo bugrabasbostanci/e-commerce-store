181db7f5627a797d446d2912f3fe53913e2a7af6
bugrabasbostanci <bb0143sbw+github@gmail.com>
2025-05-24 14:52:42 +0300

    ESLint yapılandırması eklendi, gereksiz dosyalar kaldırıldı ve yeni bileşenler ile UI bileşenleri oluşturuldu. Ayrıca, bağımlılıklar güncellendi ve bazı stil ayarları yapıldı.

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from "react";
+import { DashboardNavigation } from "../components/dashboard/DashboardNavigation";
+import { SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { MenuIcon, CircleUserIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+export default function DashboardLayout({children}: {children: React.ReactNode}) {
+    return (
+       <div className="flex w-full flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+            <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+            <DashboardNavigation />
+            </nav>
+
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button className="shrink-0 md:hidden" variant="outline" size="icon">
+                        <MenuIcon className="w-5 h-5"></MenuIcon>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="w-full max-w-md">
+                    <nav className="flex flex-col gap-6 text-lg font-medium mt-5">
+                        <DashboardNavigation />
+                    </nav>
+                </SheetContent>
+            </Sheet>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="secondary" size="icon" className="rounded-full">
+                        <CircleUserIcon className="w-5 h-5"></CircleUserIcon>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </header>
+        <main className="flex-1">{children}</main>
+       </div>
+    );
+} 
