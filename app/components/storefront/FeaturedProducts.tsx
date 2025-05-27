31638df1466a6e23284e0e1a3bee70cecbe88b8c
bugrabasbostanci <bb0143sbw+github@gmail.com>
2025-05-27 16:34:53 +0300

    feat: Add loading states for featured products and individual product pages; implement Skeleton and LoadingProductCard components

--- app/components/storefront/FeaturedProducts.tsx
+++ app/components/storefront/FeaturedProducts.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/app/lib/db"
-import { ProductCard } from "@/app/components/storefront/ProductCard"
+import { LoadingProductCard, ProductCard } from "@/app/components/storefront/ProductCard"
+import { Suspense } from "react"
 
 async function getData() {
     const data = await prisma.product.findMany({
@@ -25,17 +26,37 @@ async function getData() {
 }
 
 
-export async function FeaturedProducts() {
-    const data = await getData()
-
+export function FeaturedProducts() {
     return (
         <>
             <h2 className="text-2xl font-extrabold tracking-tight">Featured Items</h2>
-            <div className="mt-5 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                {data.map((item)=> (
-                    <ProductCard key={item.id} item={item}/>
-                ))}
-            </div>
+            <Suspense fallback={<LoadingRows />}>
+                <LoadFeaturedProducts />
+            </Suspense>
         </>
+    )
+}
+
+
+async function LoadFeaturedProducts() {
+    const data = await getData()
+
+    return (
+        <div className="mt-5 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
+            {data.map((item) => (
+                <ProductCard key={item.id} item={item} />
+            ))}
+        </div>
+    )
+}
+
+
+function LoadingRows() {
+    return (
+        <div className="mt-5 grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
+            <LoadingProductCard />
+            <LoadingProductCard />
+            <LoadingProductCard />
+        </div>
     )
 }
